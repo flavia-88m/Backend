8d0aee29c4460eeeda01cf64e83e631c14329605
flavia <fla_m19@hotmail.com>
2021-10-04 00:04:09 -0300

    Desafio clase 2

---
+++ js/app.js
@@ -0,0 +1,67 @@
+// Clase Usuario con sus atributos y métodos
+
+class User {
+    constructor(name, surname, pets, books){
+        this.name = name;
+        this.surname = surname;
+        this.pets = pets;
+        this.books = books;
+    }
+    getFullName() {
+       console.log(`El nombre del usuario es ${this.name} y su apellido ${this.surname}`)
+    }
+
+    addMascota() {
+       console.log(`El nombre de la mascota es ${this.pets}`)
+    }
+
+    countMascotas() {
+        console.log(`El usuario tiene ${(mascotas.length)} mascotas`)
+    }
+    
+    static nameAutor = ({nombre: 'The black cat', autor:'Edgar Allan Poe'})
+    
+    addBooks () {
+        console.log(`El nombre del libro y del autor es`, User.nameAutor) 
+    }
+
+    getBookNames() {
+        console.log(filtered)
+    }
+
+    }     
+
+const user = new User('Flavia', 'Maldonado')
+
+const userPet = new User('mia')
+
+let mascotas = ['lola', 'camilo', 'ringo'];
+mascotas.push(userPet);
+
+
+const userBooks = new User({nombre: 'The black cat', autor:'Edgar Allan Poe'})
+let libros = [
+    {
+     nombre: 'Pet sementary',
+     autor: 'Stephen King'
+    },
+    {
+     nombre: 'Doctor Sleep',
+     autor: 'Stephen King'
+    }
+];
+
+libros.push(userBooks);
+
+const filtered = libros.filter(librosName => 
+    librosName.nombre
+    );
+
+
+// Invocación de sus métodos
+
+user.getFullName();
+userPet.addMascota();
+userBooks.getBookNames();
+userBooks.addBooks();
+userPet.countMascotas();
